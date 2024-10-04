8f6d4917b194d0652f25387c8801492ceef793f3
ashutosh017 <wwwashutosh225@gmail.com>
2024-10-04 13:14:35 +0530

    first

---
+++ 1.js
@@ -0,0 +1,50 @@
+const crypto = require('crypto')
+
+// const input = '100xDevs';
+// const hash = crypto.createHash('sha256').update(input).digest('hex')
+
+// console.log(hash);
+
+
+function hashStartsWith00000(){
+    const prefix = '00000';
+    let val=0;
+    while(true){
+        const hash =  crypto.createHash('sha256').update(val.toString()).digest('hex');
+        if(hash.startsWith(prefix)){
+            return {"input":val.toString(),"hash":hash};
+        }
+        val++;
+    }
+}
+function inputStartsWtih100xDevs(){
+    const prefix = '00000';
+    const input = '100xDevs';
+    let val=0;
+    while(true){
+        const hash =  crypto.createHash('sha256').update(input+val.toString()).digest('hex');
+        if(hash.startsWith(prefix)){
+            return {"input":input+val.toString(),"hash":hash};
+        }
+        val++;
+    }
+}
+
+function findANonceForAGivenInput(input){
+    let nonce=0;
+    while(true){
+        const hash = crypto.createHash('sha256').update(input+nonce.toString()).digest('hex');
+        if(hash.startsWith('00000')){
+            return {
+                "input":input,
+                "nonce":nonce.toString(),
+                "hash":hash
+            }
+        }
+        nonce++;
+    }
+}
+console.log(hashStartsWith00000());
+console.log(inputStartsWtih100xDevs());
+console.log(findANonceForAGivenInput(`harkirat => Raman | Rs 100 | Ram => Ankit | Rs 10`))
+
