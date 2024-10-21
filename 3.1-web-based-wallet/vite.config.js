b972db9f8e119d0f3adcc7c512bebeee13971482
ashutosh017 <wwwashutosh225@gmail.com>
2024-10-21 10:08:11 +0530

    update vite config

--- 3.1-web-based-wallet/vite.config.js
+++ 3.1-web-based-wallet/vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { nodePolyfills, commonjs} from 'vite-plugin-node-polyfills'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills(), commonjs()],
+  plugins: [react(), nodePolyfills()],
   build: {
     rollupOptions: {
       external: ['ed25519-hd-key', '@solana/web3.js', 'tweetnacl'], // Mark it as an external dependency
