496f39968160f83308d76169df05587c6cd4985a
ashutosh017 <wwwashutosh225@gmail.com>
2024-10-21 09:27:03 +0530

    update vite config

--- 3.1-web-based-wallet/vite.config.js
+++ 3.1-web-based-wallet/vite.config.js
@@ -5,4 +5,9 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
+  build: {
+    rollupOptions: {
+      external: ['ed25519-hd-key'], // Mark it as an external dependency
+    },
+  },
 })
