a28fb84978fbe0f74622bb33786521ef5671b949
ashutosh017 <wwwashutosh225@gmail.com>
2024-10-23 10:11:25 +0530

    debug

--- 3.1-web-based-wallet/vite.config.js
+++ 3.1-web-based-wallet/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import commonjs from 'vite-plugin-commonjs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills()],
+  plugins: [react(), nodePolyfills(), commonjs()],
   build: {
     rollupOptions: {
-      external: ['ed25519-hd-key', '@solana/web3.js',], // Mark it as an external dependency
+      external: ['ed25519-hd-key', '@solana/web3.js'], // Mark it as an external dependency
     },
   },
 })
