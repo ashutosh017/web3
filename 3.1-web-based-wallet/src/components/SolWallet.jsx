8e651acb9769f7f4cb47c14b75e06452e7658d23
ashutosh017 <wwwashutosh225@gmail.com>
2024-10-23 08:12:11 +0530

    debug

--- 3.1-web-based-wallet/src/components/SolWallet.jsx
+++ 3.1-web-based-wallet/src/components/SolWallet.jsx
@@ -3,7 +3,7 @@ import { mnemonicToSeed, mnemonicToSeedSync } from "bip39";
 // import { derivePath } from "ed25519-hd-key";
 import {derivePath} from '../../node_modules/ed25519-hd-key/dist/index.js'
 // import { Keypair } from "@solana/web3.js";
-import {Keypair} from '../../node_modules/@solana/web3.js/lib/index'
+import {Keypair} from '../../node_modules/@solana/web3.js/lib/index.d.ts'
 import nacl from "tweetnacl";
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 import Card from "./Card";
